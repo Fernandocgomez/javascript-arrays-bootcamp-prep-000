3b6a8bf8757f83322182751f480fbf729f6313e6
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-08-20 17:48:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(){
-  chocolateBars.push("cat");
-  chocolateBars.unshift("skittles");
+
 }
