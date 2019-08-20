24b1973e5437335ae7f6ce1da9ce5d863cd7cb54
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-08-20 19:01:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,7 +40,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  var newArray = array.slice(0, newArray.length - 1)
+  var newArray = array.slice(2)
   return newArray
 }
 
