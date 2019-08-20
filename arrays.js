7663468d6a1dda8fb552911721627ed6deac5bfd
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-08-20 18:22:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,5 @@ function addElementToBeginningOfArray(array, element){
   var newArray = [element, array[0]]
   return newArray
 }
+
+arrays addElementToBeginningOfArray(array, element) does not alter the original array:
