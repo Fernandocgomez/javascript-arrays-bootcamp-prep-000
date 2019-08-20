dff50f0c082c7f261fb47ef7ea7cb7124a1b4c10
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-08-20 18:38:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,6 +11,6 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  var newArray = ['foo', array...]
+  var newArray = [element, array'...']
   return newArray
 }
